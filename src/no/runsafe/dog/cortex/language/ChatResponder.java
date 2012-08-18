311c7bfe722b3fc984bb57ad24718524295b18c1
Morten Nilsen <morten@runsafe.no>
2012-08-18 15:02:18 +0200

    Use new base worker class

--- src/no/runsafe/dog/cortex/language/ChatResponder.java
+++ src/no/runsafe/dog/cortex/language/ChatResponder.java
@@ -5,34 +5,31 @@ import no.runsafe.dog.cortex.memory.ChatTriggerRepository;
 import no.runsafe.framework.configuration.IConfiguration;
 import no.runsafe.framework.event.player.IPlayerChatEvent;
 import no.runsafe.framework.output.IOutput;
-import no.runsafe.framework.server.RunsafeServer;
 import no.runsafe.framework.server.event.player.RunsafePlayerChatEvent;
 import no.runsafe.framework.timer.IScheduler;
-import no.runsafe.framework.timer.ITimer;
+import no.runsafe.framework.timer.Worker;
 import org.bukkit.ChatColor;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Stack;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ChatResponder implements Runnable, Subsystem, IPlayerChatEvent
+public class ChatResponder extends Worker<String, String> implements Runnable, Subsystem, IPlayerChatEvent
 {
 	public ChatResponder(
 		IScheduler scheduler,
-		RunsafeServer server,
 		IConfiguration configuration,
 		ChatTriggerRepository repository,
 		Speech speechCenter,
 		IOutput output
 	)
 	{
+		super(scheduler);
 		this.scheduler = scheduler;
 		this.console = output;
-		this.worker = scheduler.createAsyncTimer(this, 10L, 10L);
 		this.config = configuration;
 		this.chatTriggerRepository = repository;
 		this.speech = speechCenter;
@@ -51,100 +48,80 @@ public class ChatResponder implements Runnable, Subsystem, IPlayerChatEvent
 		);
 		ruleCooldown = config.getConfigValueAsInt("autoresponder.cooldown.rule");
 		playerCooldown = config.getConfigValueAsInt("autoresponder.cooldown.player");
-		long delay = config.getConfigValueAsInt("autoresponder.delay");
-		if (delay > 0)
-		{
-			if(this.worker != null)
-				worker.stop();
-			worker = scheduler.createAsyncTimer(this, 10L, delay);
-		}
+		setInterval(config.getConfigValueAsInt("autoresponder.delay"));
 	}
 
 	@Override
 	public void OnPlayerChatEvent(RunsafePlayerChatEvent event)
 	{
 		console.finer(String.format("Receiving message '%s' from %s", event.getMessage(), event.getPlayer().getName()));
-		playerLastMessage.put(event.getPlayer().getName(), event.getMessage());
-		if (messageQueue.contains(event.getPlayer().getName()))
-			messageQueue.remove(event.getPlayer().getName());
-		messageQueue.push(event.getPlayer().getName());
-		console.finer(String.format("%d messages in queue", messageQueue.size()));
-		pokeWorker();
+		Push(event.getPlayer().getName(), event.getMessage());
 	}
 
 	@Override
-	public void run()
+	public void process(String player, String message)
 	{
-		if (messageQueue.empty())
-		{
-			console.finer("Stopping worker.");
-			worker.stop();
-			return;
-		}
-
-		String player = messageQueue.pop();
-		String message = playerLastMessage.get(player);
-
 		console.finer(String.format("Checking message '%s' from '%s'", message, player));
 
-		playerLastMessage.remove(player);
-		if (playerCooldown > 0 && playerCooldowns.contains(player))
+		if (!isPlayerCooldown(player))
+			for (Map.Entry<Pattern, String> rule : activeTriggers.entrySet())
+			{
+				Matcher matcher = rule.getKey().matcher(message);
+				if (matcher.matches())
+				{
+					applyRuleCooldown(rule);
+					applyPlayerCooldown(player);
+					String response = matcher.replaceAll(rule.getValue().replace("%player%", player));
+					console.fine(String.format("Sending response '%s'", response));
+					speech.Speak(response);
+					break;
+				}
+			}
+	}
+
+	private boolean isPlayerCooldown(String player)
+	{
+		if (playerCooldown > 0 && playerCooldowns.containsKey(player))
 		{
 			console.fine("Player is on cooldown.");
 			if (playerCooldowns.get(player) < new Date().getTime())
 				playerCooldowns.remove(player);
 			else
-				return;
+				return true;
 		}
-		for (Map.Entry<Pattern, String> rule : activeTriggers.entrySet())
+		return false;
+	}
+
+	private void applyRuleCooldown(Map.Entry<Pattern, String> rule)
+	{
+		if (ruleCooldown > 0)
 		{
-			Matcher matcher = rule.getKey().matcher(message);
-			if (matcher.matches())
+			console.fine("Putting rule on cooldown");
+			final Pattern pattern = rule.getKey();
+			final String response = rule.getValue();
+			Runnable callback = new Runnable()
 			{
-				if (ruleCooldown > 0)
+				@Override
+				public void run()
 				{
-					console.fine("Putting rule on cooldown");
-					final Pattern pattern = rule.getKey();
-					final String response = rule.getValue();
-					Runnable callback = new Runnable()
-					{
-						@Override
-						public void run()
-						{
-							activeTriggers.put(pattern, response);
-						}
-					};
-					activeTriggers.remove(rule.getKey());
-					scheduler.createAsyncTimer(callback, ruleCooldown);
+					activeTriggers.put(pattern, response);
 				}
-				if (playerCooldown > 0)
-				{
-					playerCooldowns.put(player, new Date().getTime() + (playerCooldown * 1000));
-				}
-				String response = matcher.replaceAll(rule.getValue().replace("%player%", player));
-				console.fine(String.format("Sending response '%s'", response));
-				speech.Speak(response);
-			}
+			};
+			activeTriggers.remove(rule.getKey());
+			scheduler.createAsyncTimer(callback, ruleCooldown);
 		}
 	}
 
-	private void pokeWorker()
+	private void applyPlayerCooldown(String player)
 	{
-		if (worker == null)
-			console.fine("Worker is null");
-		else
-			console.fine("Worker is " + (worker.isRunning() ? "" : "not ") + "running");
-		if (worker != null && !worker.isRunning())
-			worker.start();
+		if (playerCooldown > 0)
+			playerCooldowns.put(player, new Date().getTime() + (playerCooldown * 1000));
 	}
 
 	private IScheduler scheduler;
 	private ChatTriggerRepository chatTriggerRepository;
-	private ConcurrentHashMap<String, Long> playerCooldowns = new ConcurrentHashMap<String, Long>();
-	private ConcurrentHashMap<Pattern, String> activeTriggers = new ConcurrentHashMap<Pattern, String>();
-	private ConcurrentHashMap<String, String> playerLastMessage = new ConcurrentHashMap<String, String>();
-	private Stack<String> messageQueue = new Stack<String>();
-	private ITimer worker;
+	private HashMap<String, Long> playerCooldowns = new HashMap<String, Long>();
+	private HashMap<Pattern, String> activeTriggers = new HashMap<Pattern, String>();
 	private IConfiguration config;
 	private int ruleCooldown;
 	private int playerCooldown;

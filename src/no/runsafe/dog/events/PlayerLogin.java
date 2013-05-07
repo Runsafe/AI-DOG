252fd631b67b3bf2ac43e4869798cca0eb2eb648
Kruithne <kruithne@gmail.com>
2013-05-07 08:35:46 +0100

    Moved welcome messge to after the player has logged in, leaving the check before the log-in.

--- src/no/runsafe/dog/events/PlayerLogin.java
+++ src/no/runsafe/dog/events/PlayerLogin.java
@@ -1,12 +1,17 @@
 package no.runsafe.dog.events;
 
 import no.runsafe.dog.cortex.language.Speech;
+import no.runsafe.framework.event.player.IPlayerJoinEvent;
 import no.runsafe.framework.event.player.IPlayerPreLoginEvent;
 import no.runsafe.framework.server.RunsafeServer;
+import no.runsafe.framework.server.event.player.RunsafePlayerJoinEvent;
 import no.runsafe.framework.server.event.player.RunsafePlayerPreLoginEvent;
 import no.runsafe.framework.server.player.RunsafePlayer;
 
-public class PlayerLogin implements IPlayerPreLoginEvent
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerLogin implements IPlayerPreLoginEvent, IPlayerJoinEvent
 {
 	public PlayerLogin(Speech speechCenter)
 	{
@@ -20,8 +25,20 @@ public class PlayerLogin implements IPlayerPreLoginEvent
 		RunsafePlayer player = RunsafeServer.Instance.getPlayer(playerName);
 
 		if (player.isNew())
-			speechCenter.Speak("Welcome to the server, " + playerName);
+			this.playersToWelcome.add(playerName);
+	}
+
+	@Override
+	public void OnPlayerJoinEvent(RunsafePlayerJoinEvent event)
+	{
+		String playerName = event.getPlayer().getName();
+		if (this.playersToWelcome.contains(playerName))
+		{
+			this.speechCenter.Speak(String.format("Welcome to the server, %s.", playerName));
+			this.playersToWelcome.remove(playerName);
+		}
 	}
 
 	private Speech speechCenter;
+	private List<String> playersToWelcome = new ArrayList<String>();
 }

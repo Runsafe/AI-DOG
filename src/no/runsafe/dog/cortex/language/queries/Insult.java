afc119011ced05c22bedda14caa235526d534acb
Kruithne <kruithne@gmail.com>
2013-07-10 05:26:53 +0100

    Correctly order the insult tiers.

--- src/no/runsafe/dog/cortex/language/queries/Insult.java
+++ src/no/runsafe/dog/cortex/language/queries/Insult.java
@@ -4,10 +4,7 @@ import no.runsafe.dog.cortex.language.ChatResponderRule;
 import no.runsafe.framework.api.IConfiguration;
 import no.runsafe.framework.api.event.plugin.IConfigurationChanged;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -43,9 +40,15 @@ public class Insult extends ChatResponderRule implements IConfigurationChanged
 	public void OnConfigurationChanged(IConfiguration configuration)
 	{
 		this.insultTiers.clear();
+
+		List<String> sortOrder = new ArrayList<String>();
 		Map<String, List<String>> insults = configuration.getConfigSectionsAsList("insults");
-		for (List<String> insultTier : insults.values())
-			this.insultTiers.add(0, insultTier);
+
+		sortOrder.addAll(insults.keySet());
+		Collections.sort(sortOrder);
+
+		for (String tier : sortOrder)
+			this.insultTiers.add(insults.get(tier));
 	}
 
 	private static final Pattern question = Pattern.compile("(?i).*(darn|fuck|screw|damn)\\s(you\\s|off\\s|)dog.*");

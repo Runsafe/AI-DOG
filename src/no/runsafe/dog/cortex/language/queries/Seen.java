62ff4e91e6a0b0c4833d19a3333a749069e8b523
Morten Nilsen <morten@runsafe.no>
2013-12-16 20:23:29 +0100

    Remove internal API usage

--- src/no/runsafe/dog/cortex/language/queries/Seen.java
+++ src/no/runsafe/dog/cortex/language/queries/Seen.java
@@ -5,9 +5,8 @@ import no.runsafe.framework.RunsafePlugin;
 import no.runsafe.framework.api.IServer;
 import no.runsafe.framework.api.command.ICommandHandler;
 import no.runsafe.framework.api.command.IPreparedCommand;
+import no.runsafe.framework.api.player.IAmbiguousPlayer;
 import no.runsafe.framework.api.player.IPlayer;
-import no.runsafe.framework.internal.command.PreparedAsynchronousCommand;
-import no.runsafe.framework.minecraft.player.RunsafeAmbiguousPlayer;
 
 import java.util.regex.Matcher;
 
@@ -23,18 +22,15 @@ public class Seen extends ChatResponderRule
 	public String getResponse(String player, Matcher message)
 	{
 		IPlayer who = server.getPlayer(message.group(2).toLowerCase());
-		if (who == null || (who instanceof RunsafeAmbiguousPlayer))
+		if (who == null || (who instanceof IAmbiguousPlayer))
 			return null;
 
 		ICommandHandler command = getSeenCommand();
 		if (command == null)
 			return null;
 
-		IPreparedCommand seen = command.prepare(server.getPlayerExact(player), new String[]{who.getName()});
-		if (seen instanceof PreparedAsynchronousCommand)
-			return ((PreparedAsynchronousCommand) seen).executeDirect();
-		else
-			return seen.execute();
+		IPreparedCommand seen = command.prepare(server.getPlayerExact(player), who.getName());
+		return seen.executeDirect();
 	}
 
 	private ICommandHandler getSeenCommand()

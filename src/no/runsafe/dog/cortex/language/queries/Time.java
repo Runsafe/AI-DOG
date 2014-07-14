c60b740c053c6a47af92d1ee61fd7e99bdb6b5c9
Kruithne <kruithne@gmail.com>
2014-07-14 19:57:12 +0100

    Fix: Remove random whitespace.

--- src/no/runsafe/dog/cortex/language/queries/Time.java
+++ src/no/runsafe/dog/cortex/language/queries/Time.java
@@ -20,7 +20,7 @@ public class Time extends ChatResponderRule
 	public String getResponse(String player, Matcher message)
 	{
 		String timezone = message.group(1);
-		DateTime now =  new LocalDateTime(DateTimeZone.UTC).toDateTime(DateTimeZone.forTimeZone(TimeZone.getTimeZone(timezone)));
+		DateTime now = new LocalDateTime(DateTimeZone.UTC).toDateTime(DateTimeZone.forTimeZone(TimeZone.getTimeZone(timezone)));
 		return String.format("The time is %s:%s in %s", now.hourOfDay().getAsText(), now.minuteOfHour().getAsText(), timezone);
 	}
 }
